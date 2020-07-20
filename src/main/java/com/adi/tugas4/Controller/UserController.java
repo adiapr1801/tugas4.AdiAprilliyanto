50776565423c06aef4acf2ddac55523e4a3df31c
adiapr1801 <adi.aprilliyanto@fikom.udb.ac.id>
2020-07-20 15:37:26 +0700

    Pagination/shorting

--- src/main/java/com/adi/tugas4/Controller/UserController.java
+++ src/main/java/com/adi/tugas4/Controller/UserController.java
@@ -23,9 +23,42 @@ public class UserController {
     UserRepository userRepository;
     @Autowired
     UserService userService;
-    @Autowired
-    Pagination pagination;
     //---------------------------------------------------------------------------------------->>
+    private Sort.Direction getSortDirection(String direction) {
+        if (direction.equals("asc")) {
+            return Sort.Direction.ASC;
+        } else if (direction.equals("desc")) {
+            return Sort.Direction.DESC;
+        }
+
+        return Sort.Direction.ASC;
+    }
+    //---------------------------------------------------------------------------------------->>
+    //shorting secara descending
+    @GetMapping("/desc")
+    public ResponseEntity<List<User>> getAllTutorials(@RequestParam(defaultValue = "id,desc") String[] sort) {
+        try {
+            List<Sort.Order> orders = new ArrayList<Sort.Order>();
+            if (sort[0].contains(",")) {
+                for (String sortOrder : sort) {
+                    String[] _sort = sortOrder.split(",");
+                    orders.add(new Sort.Order(getSortDirection(_sort[1]), _sort[0]));
+                }
+            } else {
+                orders.add(new Sort.Order(getSortDirection(sort[1]), sort[0]));
+            }
+            List<User> tutorials = userRepository.findAll(Sort.by(orders));
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    //---------------------------------------------------------------------------------------->>
+    //ambil data dari userService
     @GetMapping("/ambil")
     public List<User> getAllUser(
             @RequestParam(value ="pageNo", defaultValue = "0") Integer pageNo,
